bd798af6f604d7de8e95949de42ed41b66794066
Njahi98 <njahioussama75@gmail.com>
2026-03-16 23:14:18 +0100

    feat: enhance project card component and update project page layout

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,4 @@
 import ProjectCard from "@/components/app/project-card";
-
 export default function page() {
   return (
 <main className="mx-auto min-h-screen max-w-4xl px-6 py-32">
@@ -7,13 +6,28 @@ export default function page() {
       <h1 className="mb-4 text-2xl font-semibold tracking-tight text-balance text-slate-50 sm:text-5xl md:text-4xl">
         Featured Projects
       </h1>
-       <p className="mb-2 max-w-2xl text-sm text-balance text-slate-400 sm:text-base">
+        <div className="flex flex-col gap-4">
+          
+       <p className=" max-w-2xl text-sm text-balance text-slate-400 sm:text-base">
         A variety of projects I have worked on.
        </p>
-       <p className="w-20 h-px mx-auto mt-4 bg-linear-to-r from-fuchsia-300 to-fuchsia-800 rounded-full mb-8"/>
-       <div className="flex flex-col gap-4">
-        <ProjectCard/>
-        <ProjectCard/>
+       <p className="w-20 h-px mx-auto bg-linear-to-r from-fuchsia-300 to-fuchsia-800 rounded-full mb-8"/>
+        </div>
+
+       <div className="grid md:grid-cols-2 gap-4">
+        <ProjectCard 
+        title="Enhancing ERP Project"
+        description="description"
+        image="/projects/project1/proj1S1.webp"
+        link="/projects/project1"
+        />
+        <ProjectCard 
+        title="Enhancing ERP Project"
+        description="description"
+        image="/projects/project1/proj1S1.webp"
+        link="/projects/project1"
+        />
+        
        </div>
         
       </section>
--- components/app/project-card.tsx
+++ components/app/project-card.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function ProjectCard() {
+type projectCardType = {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+}
+
+export default function ProjectCard({title,description,image,link}:projectCardType) {
   return (
-    <div className="border rounded-xl border-slate-800/70 bg-slate-950/70 shadow-lg shadow-fuchsia-500/10 backdrop-blur-2xl px-6 py-3 max-w-xl mx-auto">
-        <p className="text-slate-50 text-start mb-4">
-          projectCard</p>
-        <Image src="/projects/project1/proj1S1.webp" width={1920} height={1080} alt="project 1 showcase" className=" rounded-2xl"/>
+    <Link href={link ? link : '#'}>
+    <div className=" flex flex-col gap-4 border rounded-xl border-slate-800/70 bg-slate-950/70 shadow-lg shadow-fuchsia-500/10 backdrop-blur-2xl px-6 py-3 max-w-xl w-full mx-auto">
+        <p className="text-slate-50 text-start">
+          {title}</p>
+        <p className="text-start max-w-2xl text-sm text-slate-400">
+         {description}
+        </p>
+        <Image src={image} width={1920} height={1080} alt="project 1 showcase" className=" rounded-xl"/>
     </div>
+    </Link>
   )
 }
