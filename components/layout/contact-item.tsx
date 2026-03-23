e7971e4479eec329357daf39082d395b26c967ee
Njahi98 <njahioussama75@gmail.com>
2026-03-23 19:32:06 +0100

    feat: improve contact item component with enhanced link handling and styling

--- components/layout/contact-item.tsx
+++ components/layout/contact-item.tsx
@@ -1,25 +1,27 @@
-import { Contact } from "../data/contact-data"
+import { Contact } from "../data/contact-data";
 
 export default function ContactItem({ method, href, text, Icon }: Contact) {
-  return (
-    <div className="flex w-full items-center gap-4 rounded-lg bg-slate-800/50 p-4 tracking-tighter sm:tracking-normal sm:max-w-xs">
-      <Icon className="text-slate-50 shrink-0" size={18} />
-      <div className="flex flex-col items-start">
-        <p className="text-start text-slate-50">{method}</p>
-        {href ? (
+  return href ? (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex w-full items-center gap-2 sm:gap-4 rounded-lg bg-slate-800/50 p-4 text-sm text-slate-400 transition-colors duration-200 hover:bg-slate-800/90 hover:text-slate-200 sm:max-w-xs"
+    >
+      <Icon className="shrink-0 text-slate-50" size={18} />
 
-          <a
-            href={href}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-sm text-slate-400 hover:text-slate-200"
-          >
-            {text}
-            
-          </a>
-        ) : (
-          <p className="text-sm text-slate-400 hover:text-slate-200">{text}</p>
-        )}
+      <div className="flex flex-col items-start">
+        <p className="text-slate-50">{method}</p>
+        <span>{text}</span>
+      </div>
+    </a>
+  ) : (
+    <div className="flex w-full items-center gap-4 rounded-lg bg-slate-800/50 p-4 sm:max-w-xs">
+      <Icon className="shrink-0 text-slate-50" size={18} />
+
+      <div className="flex flex-col items-start">
+        <p className="text-slate-50">{method}</p>
+        <p className="text-sm text-slate-400">{text}</p>
       </div>
     </div>
   )
